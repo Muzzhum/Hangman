e13502df065f34b6fd142c305c98356874c97255
Magnus Renaa Kjørseng <mgnusk@gmail.com>
2016-10-26 12:56:33 +0200

    Button generator works, positioning of new buttons doesn't yet. Need to figure it out in a not-so-ugly way.

--- Hangman/Form1.cs
+++ Hangman/Form1.cs
@@ -20,16 +20,61 @@ namespace Hangman
 		private void Form1_Load(object sender, EventArgs e)
 		{
 			List<Button> buttonList = new List<Button>();
-			char[] alphabet = "abcdefghijklmnopqrstuvwxyzÆØÅ".ToUpper().ToCharArray();
-			for (int i = 0; i < alphabet.Length; i++)
+			List<Char> alphabet = new List<Char>();
+			string letters = "qwertyuiopåasdfghjkløæzxcvbnm".ToUpper();
+			foreach (char c in letters) alphabet.Add(c);
+
+			int i = 0;
+			for (int j = 0; j < 3; j++)
+			{
+				for (; i < alphabet.Count; i++)
+				{
+					buttonList.Add(new Button());
+					buttonList.Last().Text = alphabet[i].ToString();
+					buttonList.Last().Location = new Point(((i%11 + 1) * 23) + 12, (j * 28) +220);
+					buttonList.Last().Height = 25;
+					buttonList.Last().Width = 20;
+					buttonList.Last().Visible = true;
+
+					this.Controls.Add(buttonList[i]);
+				}
+			}
+		/*
+			for (int i = 0; i < 11; i++)
 			{
 				buttonList.Add(new Button());
-				buttonList[i].Text = alphabet[i].ToString();
-				buttonList[i].Location = new Point(12 + (i * 12), 240);
-				buttonList[i].Height = 23;
-				buttonList[i].Width = 20;
-				buttonList[i].Visible = true;
+				buttonList.Last().Text = alphabet[i].ToString();
+				buttonList.Last().Location = new Point((i * 23) + 12,  220);
+				buttonList.Last().Height = 25;
+				buttonList.Last().Width = 20;
+				buttonList.Last().Visible = true;
+
+				this.Controls.Add(buttonList[i]);
 			}
+		for (int i = 11; i < 22; i++)
+		{
+			buttonList.Add(new Button());
+			buttonList.Last().Text = alphabet[i].ToString();
+			buttonList.Last().Location = new Point(((i -11)*23) + 12, (35) + 220);
+			buttonList.Last().Height = 25;
+			buttonList.Last().Width = 20;
+			buttonList.Last().Visible = true;
+
+			this.Controls.Add(buttonList[i]);
 		}
+		for (int i = 22; i < 29; i++)
+		{
+			buttonList.Add(new Button());
+			buttonList.Last().Text = alphabet[i].ToString();
+			buttonList.Last().Location = new Point(((i -22)*23) + 12, (2 * 35) + 220);
+			buttonList.Last().Height = 25;
+			buttonList.Last().Width = 20;
+			buttonList.Last().Visible = true;
+
+			this.Controls.Add(buttonList[i]);
+		}
+		*/
+
+	}
 	}
 }
